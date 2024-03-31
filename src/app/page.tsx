e2366b456eb9679615de3430cc09bfd285a7d3cb
masa73044 <108378047+masa73044@users.noreply.github.com>
2024-03-31 15:23:01 -0400

    added s3.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,45 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { UserButton, auth } from "@clerk/nextjs";
+import Link from "next/link";
+import { LogIn } from "lucide-react";
+import FileUpload from "@/components/ui/FileUpload";
 
-export default function Home() {
-  return <h1>testing</h1>;
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+          <p className="max-w-xl mt-1 text-1g text-slate-600">
+            Empowering sustainable decisions, our AI analyzes ESG PDF reports,
+            delivering actionable insights for a greener, more responsible
+            future
+          </p>
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <FileUpload />
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get started
+                  <LogIn className="w-4 h-4 m1-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
