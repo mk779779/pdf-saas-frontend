9965991d4eda3f16b38c28daf1a7212564616dc2
masa73044 <108378047+masa73044@users.noreply.github.com>
2024-03-31 17:16:12 -0400

    implemented Providers.tsx

--- src/components/ui/FileUpload.tsx
+++ src/components/ui/FileUpload.tsx
@@ -1,14 +1,55 @@
 "use client";
+import { uploadToS3 } from "@/lib/s3";
+import { useMutation } from "@tanstack/react-query";
 import { Inbox } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
+import axios from "axios";
 
 export default function FileUpload() {
+  const { mutate } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const response = await axios.post(`/api/create-chat`, {
+        file_key,
+        file_name,
+      });
+      return response.data;
+    },
+  });
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
-    onDrop: (acceptedFiles) => {
+    onDrop: async (acceptedFiles) => {
       console.log(acceptedFiles);
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        // larger than 10mb
+        alert("please upload a smaller file");
+        return;
+      }
+      try {
+        const data = await uploadToS3(file);
+        if (!data?.file_key || !data.file_name) {
+          alert("something went wrong");
+          return;
+        }
+        mutate(data, {
+          onSuccess: (data) => {
+            console.log(data);
+          },
+          onError: (err) => {
+            console.log(err);
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
     },
   });
   return (
@@ -23,6 +64,7 @@ export default function FileUpload() {
         <>
           <Inbox className="w-10 h-10 text-blue-500" />
           <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
+          <p className="mt-2 text-sm text-slate-400">max 10mb</p>
         </>
       </div>
     </div>
